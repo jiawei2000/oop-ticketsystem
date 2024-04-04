85d610b96ed4d17e9ad0de14ce5e6f04187e4148
Kenneth Lim <kenneth.lim.2021@smu.edu.sg>
2024-04-04 13:43:15 +0800

    ui update

--- frontend/oop/src/components/AppBar.vue
+++ frontend/oop/src/components/AppBar.vue
@@ -1,5 +1,37 @@
 <template>
-    <v-app>
+    <v-card>
+      <!-- Header and symbols -->
+      <v-toolbar color="primary">
+  
+        <v-toolbar-title>
+          <v-btn>
+            Ticketing System
+          </v-btn>
+          
+  
+        </v-toolbar-title>
         
-    </v-app>
-</template>
+
+        <v-text-field
+          w-25
+          variant="solo"
+          label="Event Search"
+          append-inner-icon="mdi-magnify"
+          single-line
+          hide-details
+          class="search-field"
+        ></v-text-field>
+        <v-btn>
+            <!-- <v-icon :icon="`mdiSvg:${mdiAccount}`"></v-icon> -->
+            Login/Register
+        </v-btn>
+  
+      </v-toolbar>
+    </v-card>
+  </template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
--- frontend/oop/src/router/index.js
+++ frontend/oop/src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
         component: () => import('@/pages/Login.vue')}
 
     ]
-  }
+  },
 ]
 
 const router = createRouter({
