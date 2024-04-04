da941a24debdc4a5250c50dae96881ccd5e7066c
dtjww <97948483+dtjww@users.noreply.github.com>
2024-04-04 15:11:43 +0800

    raw template for home

--- frontend/oop/src/pages/Home.vue
+++ frontend/oop/src/pages/Home.vue
@@ -24,4 +24,31 @@
             <p>Register to get started!</p>
         </v-card-text>
     </v-card>
+    <v-card>
+        <v-card-title>
+            <h1>Upcoming Events</h1>
+        </v-card-title>
+        <v-card-text>
+            <p>Here are some of the upcoming events:</p>
+            <ul>
+                <li>Event 1</li>
+                <li>Event 2</li>
+                <li>Event 3</li>
+            </ul>
+        </v-card-text>
+    </v-card>
+    <v-card>
+        <v-card-title>
+            <h1>Popular Events</h1>
+        </v-card-title>
+        <v-card-text>
+            <p>Here are some of the popular events:</p>
+            <ul>
+                <li>Event 1</li>
+                <li>Event 2</li>
+                <li>Event 3</li>
+            </ul>
+        </v-card-text>
+    </v-card>
+    
 </template>
