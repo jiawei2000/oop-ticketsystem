6d5436eabee3e7a56eb89ca8de9211f8db6840d5
Jiawei <jiawei.ang.2022@scis.smu.edu.sg>
2024-04-10 00:39:18 +0800

    commented auth for development

--- frontend/ticketing-system/src/router/index.js
+++ frontend/ticketing-system/src/router/index.js
@@ -20,10 +20,10 @@ const router = createRouter({
 
 router.beforeEach(to => {
     // If not logged in redirect to login page
-    if (localStorage.getItem('UserId') === null && (to.name != 'login' && to.name != 'register')) {
-        console.log("Not logged in");
-        return { name: 'login', query: to.query }
-    }
+    // if (localStorage.getItem('UserId') === null && (to.name != 'login' && to.name != 'register')) {
+    //     console.log("Not logged in");
+    //     return { name: 'login', query: to.query }
+    // }
     
 })
 
