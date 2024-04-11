9e6c7a5a06acf8c183ad940a4fbffcc24e05d02c
HongWei <hongwei.gan.2022@smu.edu.sg>
2024-04-11 21:17:18 +0800

    update eventdetail page

--- frontend/ticketing-system/src/pages/ticketOfficer/ViewEventDetails/[id].vue
+++ frontend/ticketing-system/src/pages/ticketOfficer/ViewEventDetails/[id].vue
@@ -11,7 +11,7 @@ const formatDate = (dateString) => {
   const [year, month, day] = dateString.split('-');
   return `${day}-${month}-${year}`;
 }
-
+const ticketOfficers = ref([]);
 const event = reactive({
   "eventId": "",
   "eventname": "",
@@ -39,6 +39,7 @@ onMounted(() => {
   eventId.value = route.params.id;
   console.log(eventId.value)
   getEvent();
+  getTicketOfficer();
 })
 
 const getEvent = async () => {
@@ -74,105 +75,208 @@ const getEvent = async () => {
 
 };
 
-const cancelEvent = async () =>{
+const getTicketOfficer = async () => {
+  const getTOURL = "manager/getTicketOfficers/" + eventId.value;
+  axios.get(getTOURL)
+    .then(response => {
+      const data = response.data;
+      console.log(data);
+      ticketOfficers.value = data;
+
+    })
+    .catch(error => {
+      console.log(error.message);
+    })
+}
+
+
+const cancelEvent = async () => {
   console.log("A");
 }
 
-const editEventDetails = async () =>{
+const editEventDetails = async () => {
   console.log("B");
 }
 
+const removeTicketOfficer = async (ticketOfficerId) => {
+  console.log("C", ticketOfficerId);
+  const removeTicketOfficerURL = "manager/removeTicketOfficers/" + ticketOfficerId;
+  axios.delete(removeTicketOfficerURL)
+    .then(response => {
+      const data = response.data;
+      console.log(data);
+      message.value = data;
+      getTicketOfficer(); //update table 
+      OpenModal(); //open message 
+    })
+    .catch(error => {
+      console.log(error.message);
+    })
+}
+//for modal 
+const message = ref('');
+const MsgModal = ref(false);
+const OpenModal = () => {
+  MsgModal.value = true;
+}
+const CloseModal = () => {
+  MsgModal.value = false;
+}
+
+const activeTab = ref(0);
 </script>
 
 <template>
-  <section>
-    <!-- Display Event Details -->
+  <!-- message modal -->
+  <v-dialog v-model="MsgModal" max-width="500">
     <v-card>
-      <v-card-title>Event Details of EventId: {{ event.eventId }}</v-card-title>
-      <v-card-text>
-        <!-- form (EventId, Eventname, status,  venue, date, time, price, cancellation fee, stock,  ) -->
-
-        <v-form>
-          <v-container>
-            <!-- EventName and status -->
-            <v-row>
-              <v-col cols=8>
-                <v-text-field v-model="event.eventname" label="Event Name" readonly></v-text-field>
-              </v-col>
-              <v-col cols=4>
-                <v-text-field v-model="event.status" label="Status" readonly></v-text-field>
-              </v-col>
-            </v-row>
-            <!-- Date and time -->
-            <v-row>
-              <v-col cols=6>
-                <v-text-field v-model="event.date" label="Date" readonly></v-text-field>
-              </v-col>
-              <v-col cols=6>
-                <v-text-field v-model="event.time" label="Time" readonly></v-text-field>
-              </v-col>
-            </v-row>
-            <!-- Price, Cancellation Fee, Stock -->
-            <v-row>
-              <v-col cols=4>
-                <v-text-field v-model="event.price" label="Price($)" readonly></v-text-field>
-              </v-col>
-              <v-col cols=4>
-                <v-text-field v-model="event.cancellationFee" label="Cancellation Fee($)" readonly></v-text-field>
-              </v-col>
-              <v-col cols=4>
-                <v-text-field v-model="event.stock" label="Stock" readonly></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-form>
+      <v-card-title>
+        <span class="headline">Message</span>
+      </v-card-title>
+      <v-card-text class="text-center">
+        <p><v-icon class="success-icon">mdi-checkbox-marked-circle-outline</v-icon>{{ message }}</p>
+        <!-- <p v-else><v-icon class="error-icon">mdi-alert-octagon-outline</v-icon>{{ message }}</p> -->
       </v-card-text>
-      <v-card-actions class="justify-end">
-        <v-btn variant="outlined" secondary="error" @click = editEventDetails >Edit Details</v-btn>
-        <v-btn variant="outlined" color="error" @click = cancelEvent >Cancel</v-btn>
+      <v-card-actions class="justify-center">
+        <v-btn color="primary" @click="CloseModal">Close</v-btn>
       </v-card-actions>
     </v-card>
+  </v-dialog>
+  <v-card>
+    <!-- Display Event Details -->
+    <v-tabs v-model="activeTab">
+      <v-tab value="one">Event Details</v-tab>
+      <v-tab value="two">Ticket Officers</v-tab>
+    </v-tabs>
 
-    <!-- Event statistics -->
-    <v-card class = "my-6">
-      <v-card-title>Event Statistics</v-card-title>
-      <v-card-text>
-        <!-- form (EventId, Eventname, status,  venue, date, time, price, cancellation fee, stock,  ) -->
-        <v-form>
-          <v-container>
-            <!-- Total sales -->
-            <v-row>
-              <v-col>
-                <v-text-field v-model="eventStatistics.totalRevenue" label="Total Revenue" readonly></v-text-field>
-              </v-col>
-            </v-row>
-            <!-- sales rev, refund rev, sales -->
-            <v-row>
-              <v-col cols=5>
-                <v-text-field v-model="eventStatistics.saleRevenue" label="Sales Revenue" readonly></v-text-field>
-              </v-col>
-              <v-col cols=5>
-                <v-text-field v-model="eventStatistics.refundRevenue" label="Refund Revenue" readonly></v-text-field>
-              </v-col>
-              <v-col cols=2>
-                <v-text-field v-model="eventStatistics.sales" label="Sales" readonly></v-text-field>
-              </v-col>
-            </v-row>
-            <!-- Price, Cancellation Fee, Stock -->
-            <v-row>
-              <v-col cols=4>
-                <v-text-field v-model="eventStatistics.attendance" label="Attendance" readonly></v-text-field>
-              </v-col>
-              <v-col cols=4>
-                <v-text-field v-model="eventStatistics.noRefunds" label="Number of refunds" readonly></v-text-field>
-              </v-col>
-              <v-col cols=4>
-                <v-text-field v-model="eventStatistics.noCancelled" label="Number of tickets to cancel" readonly></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-form>
-      </v-card-text>
-    </v-card>
-  </section>
+    <v-window v-model="activeTab">
+      <!-- Tab1 -->
+      <v-window-item value="one">
+        <!-- Event details -->
+        <v-card class="my-6 mx-6">
+          <v-card-title>EventId: {{ event.eventId }}</v-card-title>
+          <v-card-text>
+            <!-- form (EventId, Eventname, status,  venue, date, time, price, cancellation fee, stock,  ) -->
+
+            <v-form>
+              <v-container>
+                <!-- EventName and status -->
+                <v-row>
+                  <v-col cols=8>
+                    <v-text-field v-model="event.eventname" label="Event Name" readonly></v-text-field>
+                  </v-col>
+                  <v-col cols=4>
+                    <v-text-field v-model="event.status" label="Status" readonly></v-text-field>
+                  </v-col>
+                </v-row>
+                <!-- Date and time -->
+                <v-row>
+                  <v-col cols=6>
+                    <v-text-field v-model="event.date" label="Date" readonly></v-text-field>
+                  </v-col>
+                  <v-col cols=6>
+                    <v-text-field v-model="event.time" label="Time" readonly></v-text-field>
+                  </v-col>
+                </v-row>
+                <!-- Price, Cancellation Fee, Stock -->
+                <v-row>
+                  <v-col cols=4>
+                    <v-text-field v-model="event.price" label="Price($)" readonly></v-text-field>
+                  </v-col>
+                  <v-col cols=4>
+                    <v-text-field v-model="event.cancellationFee" label="Cancellation Fee($)" readonly></v-text-field>
+                  </v-col>
+                  <v-col cols=4>
+                    <v-text-field v-model="event.stock" label="Stock" readonly></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn variant="outlined" secondary="error" @click=editEventDetails>Edit Details</v-btn>
+            <v-btn variant="outlined" color="error" @click=cancelEvent>Cancel</v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <!-- Event statistics -->
+        <v-card class="my-6 mx-6">
+          <v-card-title>Event Statistics</v-card-title>
+          <v-card-text>
+            <!-- form (EventId, Eventname, status,  venue, date, time, price, cancellation fee, stock,  ) -->
+            <v-form>
+              <v-container>
+                <!-- Total sales -->
+                <v-row>
+                  <v-col>
+                    <v-text-field v-model="eventStatistics.totalRevenue" label="Total Revenue" readonly></v-text-field>
+                  </v-col>
+                </v-row>
+                <!-- sales rev, refund rev, sales -->
+                <v-row>
+                  <v-col cols=5>
+                    <v-text-field v-model="eventStatistics.saleRevenue" label="Sales Revenue" readonly></v-text-field>
+                  </v-col>
+                  <v-col cols=5>
+                    <v-text-field v-model="eventStatistics.refundRevenue" label="Refund Revenue"
+                      readonly></v-text-field>
+                  </v-col>
+                  <v-col cols=2>
+                    <v-text-field v-model="eventStatistics.sales" label="Sales" readonly></v-text-field>
+                  </v-col>
+                </v-row>
+                <!-- Price, Cancellation Fee, Stock -->
+                <v-row>
+                  <v-col cols=4>
+                    <v-text-field v-model="eventStatistics.attendance" label="Attendance" readonly></v-text-field>
+                  </v-col>
+                  <v-col cols=4>
+                    <v-text-field v-model="eventStatistics.noRefunds" label="Number of refunds" readonly></v-text-field>
+                  </v-col>
+                  <v-col cols=4>
+                    <v-text-field v-model="eventStatistics.noCancelled" label="Number of tickets to cancel"
+                      readonly></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-window-item>
+
+      <!-- Tab2 -->
+      <v-window-item value="two">
+        <VCard>
+          <v-card-title>Ticket Officers</v-card-title>
+          <VCardText>
+            <VDivider />
+            <VTable class="text-no-wrap">
+              <!-- TicketId, EventName, Username, Type, Status, Actions -->
+              <thead>
+                <tr>
+                  <th>TicketOfficerId</th>
+                  <th>Username</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr v-for="ticketOfficer in ticketOfficers" :key="ticketOfficer.userId">
+                  <td>{{ ticketOfficer.userId }}</td>
+                  <td>{{ ticketOfficer.userName }}</td>
+                  <td>
+                    <v-btn variant="outlined" color="error"
+                      @click="removeTicketOfficer(ticketOfficer.userId)">Remove</v-btn>
+                  </td>
+                </tr>
+              </tbody>
+            </VTable>
+          </VCardText>
+          <v-card-actions class="justify-end">
+            <v-btn  @click=cancelEvent>Add Ticket Officer</v-btn>
+          </v-card-actions>
+        </VCard>
+      </v-window-item>
+    </v-window>
+  </v-card>
 </template>
